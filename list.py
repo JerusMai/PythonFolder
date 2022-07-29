b5403a9182f8fe1602205e6a31ac7702d2d19ac5
Jerus Mai <maikonej@gmail.com>
2022-07-29 10:19:59 +0100

    commit here

---
+++ list.py
@@ -0,0 +1,11 @@
+myList = [1, 2, 3, 4, 5, 'Hello'] 
+#print the entire list. 
+print(myList) 
+print(myList[0])
+print(myList[1])
+print(myList[4])
+print(myList[5])
+print(myList[-2])
+
+
+#You’ll get [1, 2, 3, 4, 5, “Hello”]
